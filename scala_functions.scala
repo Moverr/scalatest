9373e81e80acad7d2c423f9828984fcb138ed52b
Muyinda Rogers <moverr@gmail.com>
2019-11-13 16:52:32 +0300

    :Debugging :

--- scala_functions.scala
+++ scala_functions.scala
@@ -1,7 +1,22 @@
-def add(num1:Int,num2:Int):String = {
+def add(num1:Int,num2:Int):Double = {
    
     return  (num1 +num2); 
      
+} 
+
+def isPrime(numCheck:Int): Boolean ={
+
+    for(n <- Range(2,numCheck)){
+        if(numCheck % n == 0 ){
+            return false;
+        }
+    }
+    return true;
 }
 
-add(3,2)
+ val numbers = List(1,2,3,7,8,1,2,3,3,4,7,34,5,6,7)
+ def removeDuplicates(nums:List[Int]): Set[Int]={
+     return nums.toSet;
+ }
+
+ removeDuplicates(numbers);

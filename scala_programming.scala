cd2bd46bf5e3aeb13b7fcc2e7fd0cd8f99e0fd06
Muyinda Rogers <moverr@gmail.com>
2019-11-13 15:09:23 +0300

    IfElse scenarios

---
+++ scala_programming.scala
@@ -0,0 +1,10 @@
+var x:String = "Hello"
+if(x.contains("lrrl")){
+    println("Interesting Programing")
+}else{
+     println("Wonderful Programing")
+}
+
+var salary:Double = 123.44
+
+println("Salary is ${salary}")
